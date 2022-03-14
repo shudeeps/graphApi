c1f46469d1cd261a47ba4bc58ec39f14d6e88e83
sudip stha <shudeep86@gmail.com>
2022-03-14 21:04:21 +0000

    emal javascript view

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,47 +1,69 @@
 @extends('viewmessage')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 
 @section('content')
     <h1>View Message</h1>
 
-    <table class="table">
+    <table class="table" id="viewmessage">
         <thead>
         <tr>
             <th scope="col">User Name</th>
             <th scope="col">Message Title Subject</th>
             <th scope="col">Email Address</th>
             <th scope="col">Body</th>
-            <th scope="col">Date and Time</th>
+            <th scope="col">Full details</th>
             <th scope="col">Please Reply</th>
         </tr>
         </thead>
         @foreach ($eventss as $message)
 
+      
             <tr>
                 <td><?php echo $message->getSender()->getEmailAddress()->getName() ?></td>
                 <td><?php echo $message->getSubject() ?></td>
                 <td><?php echo $message->getSender()->getEmailAddress()->getAddress() ?></td>
                 <td><?php echo $message->getbodypreview() ?></td>
-
+                <td>
+                <button type="button"  data-toggle="modal" data-target="#myModal" class="btn btn-success"  onclick='myFunction(<?php  echo json_encode($message); ?>)'>View</button>
 
                 <td>
 
 
-                <!-- sdfds -->
-
-
-                    <form action="{{route('messageSend')}}" method="POST">
+                    <form action="{{route('messageSend')}}" id="hidden_form" method="POST">
                         {{ csrf_field() }}
                         <input type="hidden" class="form-control" name="eventuser" value={{ $message->getSender()->getEmailAddress()->getName()}} />
-                        <input type="hidden" class="form-control" name="eventsubject" value={{$message->getSubject()}} ' />
+                        <input type="hidden" class="form-control" name="eventsubject" value="{{$message->getSubject()}}" />
                         <input type="hidden" class="form-control" name="eventemail" value={{$message->getSender()->getEmailAddress()->getAddress()}} ' />
-                        <input type="hidden" class="form-control" name="eventBody" value="{{$message->getbodypreview()}}"/>
+                        <input type="hidden" class="form-control" name="eventBody" value="{{$message->getbodypreview()}}"  />
                         <button type="submit" class="btn btn-primary">
                             send
                         </button>
+
                     </form>
+
 
             </tr>
             @endforeach
             </table>
     </form>
 @endsection
+
+<script>
+function myFunction(message) {
+
+    console.log(message['receivedDateTime']);
+
+    $('#myModal').modal('show');
+    var name=message['sender']['emailAddress']['name'];
+    var subject=message['subject'];
+
+    console.log(subject);
+
+
+
+    $("#myModal .modal-body").html('username:    '+name+'<br>'+ 'Subject :'+subject+'');
+
+
+}
+</script>
--- resources/views/viewmessage.blade.php
+++ resources/views/viewmessage.blade.php
@@ -11,6 +11,26 @@
 </head>
 
 <body>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container">
         <a href="/" class="navbar-brand">PHP Graph Tutorial Amrit Gurung</a>
@@ -65,6 +85,8 @@
         </div>
     </div>
 </nav>
+
+
 <main role="main" class="container">
 
     @yield('content')
